4d3604c03336a605d65af263413823848a10cd0a
Yurioshi <55996763+Yurioshi@users.noreply.github.com>
2019-12-19 16:04:51 -0300

    Versão 0.12

--- Assets/Scripts/Movimentacao.cs
+++ Assets/Scripts/Movimentacao.cs
@@ -15,17 +15,16 @@ public class Movimentacao : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-
+        
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector2(speed * movement.x, 0), ForceMode2D.Force);
-        rb.AddForce(new Vector2(0, speed * movement.y), ForceMode2D.Force);
-
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+        
+        rb.MovePosition(transform.position * speed * movement * Time.deltaTime);
     }
 }
